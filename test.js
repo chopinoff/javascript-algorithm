e203be70ecd3e01a433983f1b5c987af616a30aa
HY KIM <chopinoff@gmail.com>
2024-02-23 23:19:44 +0900

    240223_Daily Algorithm 1

--- test.js
+++ test.js
@@ -1,10 +1,15 @@
-const str = "Let's meet at 11:50 am";
-console.log(str.split(/:| |'/)); // []
-console.log(Number(true));
-console.log(Number(null));
-const map = new Map([['key', 'value']]);
-console.log(map.get(1));
-console.log(Number(map.get(1)));
-console.log(map.get('key'));
-const arr = [1, 2, 3];
-console.log(arr?.[3] || 0);
+function solution(numbers) {
+    const newNums = numbers.map((n) => String(n));
+    const sortNums = (a, b) => {
+        const n = Math.min(a.length, b.length);
+        for (let i = 0; i < n; i++) {
+            if (a[i] !== b[i]) return b[i] - a[i];
+        }
+        console.log(a[n], b[n], a[0], a.length, b.length);
+        return a[n] || b[n] > a[0] ? b.length - a.length : a.length - b.length;
+    };
+    newNums.sort((a, b) => sortNums(a, b));
+    return newNums.join('');
+}
+
+console.log(solution([3, 30, 34]));
